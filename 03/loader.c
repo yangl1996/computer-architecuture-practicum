0731a8cd72be9a97d5ab730de087d7438836d6d8
Lei Yang <yltt1234512@gmail.com>
2016-11-04 21:59:50 +0800

    loader

---
+++ 03/loader.c
@@ -0,0 +1,173 @@
+#include "loader.h"
+
+int loadtable64_num;
+Load_table64* loadtable64;
+int loadtable32_num;
+Load_table32* loadtable32;
+
+int load(const char* path)
+{
+    /* open elf */
+    FILE* input_file = fopen(path, "rb");
+    if (!input_file)
+    {
+        /* failed to open file */
+        fclose(input_file);
+        return 0;
+    }
+
+    /* check magic number to tell whether it is 32-bit or 64-bit */
+    unsigned char elf_magic[EI_NIDENT];
+    fread((void *)elf_magic, sizeof(unsigned char) * EI_NIDENT, 1, input_file);
+    /* sanity checks */
+    if (elf_magic[EI_MAG0] == ELFMAG0 &&
+        elf_magic[EI_MAG1] == ELFMAG1 &&
+        elf_magic[EI_MAG2] == ELFMAG2 &&
+        elf_magic[EI_MAG3] == ELFMAG3 &&
+        elf_magic[EI_DATA] == ELFDATA2LSB &&
+        elf_magic[EI_OSABI] == ELFOSABI_LINUX)
+    {
+        /* check elf bit depth */
+        if (elf_magic[EI_CLASS] == ELFCLASS32)
+        {
+            return load32(input_file);
+        }
+        else if (elf_magic[EI_CLASS] == ELFCLASS64)
+        {
+            return load64(input_file);
+        }
+    }
+    fclose(input_file);
+    return 0;
+}
+
+int load64(FILE* input_file)
+{
+    /* read elf header */
+    rewind(input_file);
+    Elf64_Ehdr elf_header;
+    fread(&elf_header, sizeof(Elf64_Ehdr), 1, input_file);
+ 
+    /* sanity check */
+    if (elf_header.e_type != ET_EXEC)
+    {
+        fclose(input_file);
+        return 0;
+    }
+
+    /* read program header */
+    fseek(input_file, (long int)elf_header.e_phoff, SEEK_SET);
+    Elf64_Phdr* program_headers = (Elf64_Phdr*) malloc((size_t)(elf_header.e_phentsize * elf_header.e_phnum));
+    fread(program_headers, (size_t)(elf_header.e_phentsize), (size_t)(elf_header.e_phnum), input_file);
+    
+    /* allocate memory for ELF segments */
+    int i;
+    loadtable64_num = 0;
+    /* check how many segments are loadable */
+    for (i = 0; i < elf_header.e_phnum; i++)
+    {
+        if (program_headers[i].p_type == PT_LOAD)
+        {
+            loadtable64_num += 1;
+        }
+    }
+    loadtable64 = (Load_table64*) malloc(sizeof(Load_table64) * loadtable64_num);
+    /* read program headers and load ELF */
+    int loadtable64_idx = 0;
+    for (i = 0; i < elf_header.e_phnum; i++)
+    {
+        if (program_headers[i].p_type != PT_LOAD)
+        {
+            /* not a loadable segment, pass */
+            continue;
+        }
+        loadtable64[loadtable64_idx].orig_addr = program_headers[i].p_vaddr;
+        loadtable64[loadtable64_idx].size = program_headers[i].p_memsz; /* use memsz since sections like .bss occupies no space in file but the memory must be reserved when loading */
+        loadtable64[loadtable64_idx].start_ptr = (uint8_t*) malloc((size_t)program_headers[i].p_memsz);
+        /* read file into memory */
+        fread(loadtable64[loadtable64_idx].start_ptr, sizeof(uint8_t), (size_t)program_headers[i].p_memsz, input_file);
+        loadtable64_idx += 1;
+    }
+    
+    /* do cleaning up */
+    free(program_headers);
+    fclose(input_file);
+    return 1;
+}
+
+int cleanup64()
+{
+    int i;
+    for (i = 0; i < loadtable64_num; i++)
+    {
+        free(loadtable64[i].start_ptr);
+    }
+    free(loadtable64);
+    return 1;
+}
+
+int load32(FILE* input_file)
+{
+    /* read elf header */
+    rewind(input_file);
+    Elf32_Ehdr elf_header;
+    fread(&elf_header, sizeof(Elf32_Ehdr), 1, input_file);
+ 
+    /* sanity check */
+    if (elf_header.e_type != ET_EXEC)
+    {
+        fclose(input_file);
+        return 0;
+    }
+
+    /* read program header */
+    fseek(input_file, (long int)elf_header.e_phoff, SEEK_SET);
+    Elf32_Phdr* program_headers = (Elf32_Phdr*) malloc((size_t)(elf_header.e_phentsize * elf_header.e_phnum));
+    fread(program_headers, (size_t)(elf_header.e_phentsize), (size_t)(elf_header.e_phnum), input_file);
+    
+    /* allocate memory for ELF segments */
+    int i;
+    loadtable32_num = 0;
+    /* check how many segments are loadable */
+    for (i = 0; i < elf_header.e_phnum; i++)
+    {
+        if (program_headers[i].p_type == PT_LOAD)
+        {
+            loadtable32_num += 1;
+        }
+    }
+    loadtable32 = (Load_table32*) malloc(sizeof(Load_table32) * loadtable32_num);
+    /* read program headers and load ELF */
+    int loadtable32_idx = 0;
+    for (i = 0; i < elf_header.e_phnum; i++)
+    {
+        if (program_headers[i].p_type != PT_LOAD)
+        {
+            /* not a loadable segment, pass */
+            continue;
+        }
+        loadtable32[loadtable32_idx].orig_addr = program_headers[i].p_vaddr;
+        loadtable32[loadtable32_idx].size = program_headers[i].p_memsz; /* use memsz since sections like .bss occupies no space in file but the memory must be reserved when loading */
+        loadtable32[loadtable32_idx].start_ptr = (uint8_t*) malloc((size_t)program_headers[i].p_memsz);
+        /* read file into memory */
+        fread(loadtable32[loadtable32_idx].start_ptr, sizeof(uint8_t), (size_t)program_headers[i].p_memsz, input_file);
+        loadtable32_idx += 1;
+    }
+    
+    /* do cleaning up */
+    free(program_headers);
+    fclose(input_file);
+    return 1;
+}
+
+int cleanup32()
+{
+    int i;
+    for (i = 0; i < loadtable32_num; i++)
+    {
+        free(loadtable32[i].start_ptr);
+    }
+    free(loadtable32);
+    return 1;
+}
+
---
+++ 03/loader.h
@@ -0,0 +1,56 @@
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <stdint.h>
+
+/*
+ * Load an ELF file
+ *
+ * path: pointer to a string specifying the path of file to load
+ *
+ * Return value: 1 for success, 0 for failed.
+ */
+int load(const char* path);
+
+/*
+ * Load an N-bit ELF, these functions should be invoked by load()
+ *
+ * input_file: pointer to a file object containing the ELF file
+ *
+ * Return value: 1 for success, 0 for failed
+ */
+int load32(FILE* input_file);
+int load64(FILE* input_file);
+
+/*
+ * Convert an address in ELF to acutal address
+ *
+ * addr: address to convert
+ *
+ * Return value: a void* pointing to the actual address, or NULL if error
+ */
+
+int getptr32(uint32_t addr);
+int getptr64(uint64_t addr);
+
+/*
+ * Clean up spaces occupied by load table and ELF
+ */
+int cleanup32();
+int cleanup64();
+
+typedef struct
+{
+    uint8_t* start_ptr; /* pointer to the start of segment in host memory */
+    Elf64_Addr orig_addr; /* address of the start of segment in ELF */
+    uint64_t size;      /* size of the segment in host memory */
+} Load_table64;
+
+typedef struct
+{
+    uint8_t* start_ptr; /* pointer to the start of segment in host memory */
+    Elf32_Addr orig_addr; /* address of the start of segment in ELF */
+    uint32_t size;      /* size of the segment in host memory */
+} Load_table32;
