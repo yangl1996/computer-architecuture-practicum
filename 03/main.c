a083fc42fe765029cb4b37ddcab50e2f893901a8
luyangzhu <tzzly@pku.edu.cn>
2016-11-12 18:56:18 +0800

    update main, Makefile

--- 03/main.c
+++ 03/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -10,13 +11,17 @@ int main(int argc, char *argv[])
         return 0;
     }
     Rpc = load(argv[1]);
+    Reg[2] = 0x0000000042dcdb50; //initialize stack pointer sp
 #ifdef DEBUG
     printf("Program entry point at virtual %llx\n", Rpc);
 #endif
+    Exit_flag  = 0;
     while (1)
     {
         pc = getptr64(Rpc);
         decode();
+        if(Exit_flag == 1)
+            break;
     }
     cleanup64();
 }
